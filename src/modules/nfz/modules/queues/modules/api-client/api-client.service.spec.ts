8832158264772fc61b8303893548da78eca2b1a5
Robert Dorna <rdorna8@gmail.com>
2023-09-13 01:24:39 +0200

    feat: add logging to NfzQueuesApiClientService#fetchAll()

--- src/modules/nfz/modules/queues/modules/api-client/api-client.service.spec.ts
+++ src/modules/nfz/modules/queues/modules/api-client/api-client.service.spec.ts
@@ -1,13 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { LoggerService } from '@nestjs/common';
 import { NfzQueuesApiClientService } from './api-client.service';
+
+function MockedLogger() {
+  return {
+    log: jest.fn(),
+    warn: jest.fn(),
+    error: jest.fn(),
+  };
+}
 
 describe('NfzQueuesApiClientService ', () => {
   let nfzQueuesApiClientService: NfzQueuesApiClientService;
+  let logger: LoggerService;
 
   beforeEach(async () => {
+    logger = MockedLogger();
     const module: TestingModule = await Test.createTestingModule({
       providers: [NfzQueuesApiClientService],
-    }).compile();
+    })
+      .setLogger(logger)
+      .compile();
 
     nfzQueuesApiClientService = module.get<NfzQueuesApiClientService>(
       NfzQueuesApiClientService,
@@ -18,10 +31,39 @@ describe('NfzQueuesApiClientService ', () => {
     expect(nfzQueuesApiClientService).toBeDefined();
   });
 
+  it('logger should be defined', () => {
+    expect(logger).toBeDefined();
+  });
+
+  it('startup should log that dependencies are initialized (testing detail)', () => {
+    expect(logger.log).toHaveBeenCalledTimes(1);
+    expect(logger.log).toHaveBeenNthCalledWith(
+      1,
+      'RootTestModule dependencies initialized',
+      'InstanceLoader',
+    );
+  });
+
   describe('fetchAll()', () => {
     it('should return placeholder text', () => {
       expect(nfzQueuesApiClientService.fetchAll()).toBe(
         'fetch all queues to a given benefit provided by NFZ in Poland',
+      );
+    });
+
+    it('should log that it was called', () => {
+      nfzQueuesApiClientService.fetchAll();
+
+      expect(logger.log).toHaveBeenCalledTimes(2);
+      expect(logger.log).toHaveBeenNthCalledWith(
+        1,
+        'RootTestModule dependencies initialized',
+        'InstanceLoader',
+      );
+      expect(logger.log).toHaveBeenNthCalledWith(
+        2,
+        '#fetchAll()',
+        'NfzQueuesApiClientService',
       );
     });
   });
--- src/modules/nfz/modules/queues/modules/api-client/api-client.service.ts
+++ src/modules/nfz/modules/queues/modules/api-client/api-client.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class NfzQueuesApiClientService {
+  private readonly logger = new Logger(NfzQueuesApiClientService.name);
+
   fetchAll() {
+    this.logger.log('#fetchAll()');
     return 'fetch all queues to a given benefit provided by NFZ in Poland';
   }
 }
