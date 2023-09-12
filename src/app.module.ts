235b7544756e163bfce73372e6f09d017a7ac5b1
Robert Dorna <rdorna8@gmail.com>
2023-09-12 20:45:10 +0200

    feat: remove automatically generated by NestJS cli AppController and AppService

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NfzModule } from './nfz/nfz.module';
 
 @Module({
   imports: [NfzModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
