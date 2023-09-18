e370896b9409a6753cdaf32a3a55e40961fa9cba
Robert Dorna <rdorna8@gmail.com>
2023-09-18 02:29:24 +0200

    test: add tests to check no unnecessary cache writes are done by NfzQueuesService#findAll()

--- src/modules/nfz/modules/queues/queues.service.spec.ts
+++ src/modules/nfz/modules/queues/queues.service.spec.ts
@@ -4,6 +4,7 @@ import { NfzQueuesService } from './queues.service';
 import { NfzQueuesApiClientModule } from './modules/api-client/api-client.module';
 import { NfzQueuesApiClientService } from './modules/api-client/api-client.service';
 import { NfzQueuesCacheModule } from './modules/cache/cache.module';
+import { NfzQueuesCacheService } from './modules/cache/cache.service';
 import { NfzQueuesApiQuery } from './modules/api-client/interfaces/query.interface';
 import { mockedResponse } from '../../../../../test/mocks/httpService/mocked-response-1-false-endo-06-page-2';
 import { DataSource } from 'typeorm';
@@ -25,6 +26,7 @@ describe('NfzQueuesService', () => {
   const databaseName = 'test-nfz-queues-service-database.sqlite';
   let nfzQueuesService: NfzQueuesService;
   let nfzQueuesApiClientService: NfzQueuesApiClientService;
+  let nfzQueuesCacheService: NfzQueuesCacheService;
   let dataSource: DataSource;
   let query: NfzQueuesApiQuery;
 
@@ -51,6 +53,9 @@ describe('NfzQueuesService', () => {
     nfzQueuesApiClientService = module.get<NfzQueuesApiClientService>(
       NfzQueuesApiClientService,
     );
+    nfzQueuesCacheService = module.get<NfzQueuesCacheService>(
+      NfzQueuesCacheService,
+    );
     dataSource = module.get<DataSource>(DataSource);
   });
 
@@ -65,6 +70,10 @@ describe('NfzQueuesService', () => {
 
   it('nfzQueuesApiClientService should be defined', () => {
     expect(nfzQueuesApiClientService).toBeDefined();
+  });
+
+  it('nfzQueuesCacheService is defined', () => {
+    expect(nfzQueuesCacheService).toBeDefined();
   });
 
   it('dataSource should be defined', () => {
@@ -103,6 +112,89 @@ describe('NfzQueuesService', () => {
       );
       expect(nfzQueuesApiClientService.fetchAll).toHaveBeenCalledTimes(1);
       expect(nfzQueuesApiClientService.fetchAll).toHaveBeenCalledWith(query);
+    });
+
+    it('should not order cache to write the same query and queues twice', async () => {
+      const getMethod = jest.spyOn(nfzQueuesCacheService, 'get');
+      const storeMethod = jest.spyOn(nfzQueuesCacheService, 'store');
+
+      await expect(nfzQueuesService.findAll(query)).resolves.toStrictEqual(
+        mockedValues.api.fetchAll,
+      );
+      await expect(nfzQueuesService.findAll(query)).resolves.toStrictEqual(
+        mockedValues.api.fetchAll,
+      );
+
+      expect(getMethod).toHaveBeenCalledTimes(2);
+      expect(getMethod).toHaveBeenNthCalledWith(1, query);
+      expect(getMethod).toHaveBeenNthCalledWith(2, query);
+
+      expect(storeMethod).toHaveBeenCalledTimes(1);
+      expect(storeMethod).toHaveBeenNthCalledWith(
+        1,
+        query,
+        mockedValues.api.fetchAll,
+      );
+    });
+
+    it('should not order cache to write the same query and queues twice - benefit is same but uppercase', async () => {
+      const getMethod = jest.spyOn(nfzQueuesCacheService, 'get');
+      const storeMethod = jest.spyOn(nfzQueuesCacheService, 'store');
+
+      expect(query.benefit).toBeTruthy();
+      expect(query.benefit?.toUpperCase()).not.toEqual(query.benefit);
+
+      await expect(nfzQueuesService.findAll(query)).resolves.toStrictEqual(
+        mockedValues.api.fetchAll,
+      );
+      await expect(
+        nfzQueuesService.findAll({
+          ...query,
+          benefit: query.benefit?.toUpperCase(),
+        }),
+      ).resolves.toStrictEqual(mockedValues.api.fetchAll);
+
+      expect(getMethod).toHaveBeenCalledTimes(2);
+      expect(getMethod).toHaveBeenNthCalledWith(1, query);
+      expect(getMethod).toHaveBeenNthCalledWith(2, {
+        ...query,
+        benefit: query.benefit?.toUpperCase(),
+      });
+
+      expect(storeMethod).toHaveBeenCalledTimes(1);
+      expect(storeMethod).toHaveBeenNthCalledWith(
+        1,
+        query,
+        mockedValues.api.fetchAll,
+      );
+    });
+
+    it('should order cache to write same queues second time when query is different - case is different', async () => {
+      const getMethod = jest.spyOn(nfzQueuesCacheService, 'get');
+      const storeMethod = jest.spyOn(nfzQueuesCacheService, 'store');
+
+      await expect(nfzQueuesService.findAll(query)).resolves.toStrictEqual(
+        mockedValues.api.fetchAll,
+      );
+      await expect(
+        nfzQueuesService.findAll({ ...query, case: 2 }),
+      ).resolves.toStrictEqual(mockedValues.api.fetchAll);
+
+      expect(getMethod).toHaveBeenCalledTimes(2);
+      expect(getMethod).toHaveBeenNthCalledWith(1, query);
+      expect(getMethod).toHaveBeenNthCalledWith(2, { ...query, case: 2 });
+
+      expect(storeMethod).toHaveBeenCalledTimes(2);
+      expect(storeMethod).toHaveBeenNthCalledWith(
+        1,
+        query,
+        mockedValues.api.fetchAll,
+      );
+      expect(storeMethod).toHaveBeenNthCalledWith(
+        2,
+        { ...query, case: 2 },
+        mockedValues.api.fetchAll,
+      );
     });
   });
 });
